a19d415e80ff7d6c1257cc061fc5a039a5fa0f1c
AsaFan <fandesheng@isa.qa>
2017-10-12 22:19:54 +0800

    #新建项目

---
+++ src/main/java/com/asa/model/User.java
@@ -0,0 +1,7 @@
+package com.asa.model;
+
+/**
+ * Created by AsaFan on 2017/10/12.
+ */
+public class User {
+}
---
+++ src/test/java/com/asa/test/UserTest.java
@@ -0,0 +1,13 @@
+package com.asa.test;
+
+/**
+ * Created by AsaFan on 2017/10/12.
+ */
+
+/*@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath*:/applicationContext-core.xml"})
+@ActiveProfiles({"unittest","development"})
+@WebAppConfiguration
+@TransactionConfiguration(defaultRollback = false)*/
+public class UserTest {
+}
